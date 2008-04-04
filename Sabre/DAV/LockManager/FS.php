bd12c1e91ad7766067f3a267b2189938798d020a
Evert Pot <evert@rooftopsolutions.nl>
2008-04-04 19:54:36 +0000

    Added filesystem-based lockmanager

---
+++ Sabre/DAV/LockManager/FS.php
@@ -0,0 +1,163 @@
+<?php
+
+    require_once 'Sabre/DAV/LockManager.php';
+
+    /**
+     * The Lock manager allows you to handle all file-locks centrally.
+     *
+     * This Lock Manager stores all its data in the filesystem. By default it will do this in PHP's standard temporary session directory,
+     * but this can be overriden by specifiying an alternative path in the contructor
+     * 
+     * @package Sabre
+     * @subpackage DAV
+     * @version $Id$
+     * @copyright Copyright (C) 2008 Rooftop Solutions. All rights reserved.
+     * @author Evert Pot (http://www.rooftopsolutions.nl/) 
+     * @license licence http://www.freebsd.org/copyright/license.html  BSD License (4 Clause)
+     */
+    class Sabre_DAV_FS_LockManager extends Sabre_DAV_LockManager {
+
+        /**
+         * The default data directory 
+         * 
+         * @var string 
+         */
+        private $dataDir;
+
+        public function __construct($dataDir = null) {
+
+            if (is_null($dataDir)) {
+
+                $dataDir = ini_get('session.save_path') . '/sabredav';
+                if (!is_dir($dataDir)) mkdir($dataDir,0777,true);
+
+            }
+
+            $this->dataDir = $dataDir;
+
+        }
+
+        protected function getFileNameForUri($uri) {
+
+            return $this->dataDir . '/sabredav_' . md5($uri) . '.locks';
+
+        }
+
+
+        /**
+         * Returns a list of Sabre_DAV_Lock objects  
+         * 
+         * @param string $uri 
+         * @return array 
+         */
+        public function getLocks($uri) {
+
+            $locks = $this->getData($uri);
+            foreach($locks as $k=>$lock) {
+                if (time() > $lock->timeout + $lock->created) unset($locks[$k]); 
+            }
+            return $locks;
+
+        }
+
+        /**
+         * Locks a uri 
+         * 
+         * @param string $uri 
+         * @param Sabre_DAV_Lock $lockInfo 
+         * @return bool 
+         */
+        public function lock($uri,Sabre_DAV_Lock $lockInfo) {
+
+            // We're making the lock timeout 30 minutes
+            $lockInfo->timeout = 1800;
+            $lockInfo->created = time();
+
+            $locks = $this->getLocks($uri);
+            foreach($locks as $k=>$lock) {
+                if ($lock->token == $lockInfo->token) unset($locks[$k]);
+            }
+            $locks[] = $lockInfo;
+            $this->putData($uri,$resourceData);
+        }
+
+        /**
+         * Removes a lock from a uri 
+         * 
+         * @param string $uri 
+         * @param Sabre_DAV_Lock $lockInfo 
+         * @return bool 
+         */
+        public function unlock($uri,Sabre_DAV_Lock $lockInfo) {
+
+            $locks = $this->getLocks($uri);
+            foreach($locks as $k=>$lock) {
+
+                if ($lock->token == $lockInfo->token) {
+
+                    unset($locks[$k]);
+                    $this->putData($locks);
+                    return true;
+
+                }
+            }
+            return false;
+
+        }
+
+        /**
+         * Returns the stored data for a uri
+         *
+         * @param string $uri
+         * @return array 
+         */
+        protected function getData($uri) {
+
+            $path = $this->getFilenameForUri($uri);
+            if (!file_exists($path)) return array();
+
+            // opening up the file, and creating a shared lock
+            $handle = fopen($path,'r');
+            flock($handle,LOCK_SH);
+            $data = '';
+
+            // Reading data until the eof
+            while(!feof($handle)) {
+                $data.=fread($handle,8192);
+            }
+
+            // We're all good
+            fclose($handle);
+
+            // Unserializing and checking if the resource file contains data for this file
+            $data = unserialize($data);
+            if (!$data) return array();
+            return $data;
+
+        }
+
+        /**
+         * Updates the lock information
+         *
+         * @param string $uri
+         * @param array $newData 
+         * @return void
+         */
+        protected function putData($uri,array $newData) {
+
+            $path = $this->getFileNameForUri($uri);
+
+            // opening up the file, and creating a shared lock
+            $handle = fopen($path,'a+');
+            flock($handle,LOCK_EX);
+            ftruncate($handle,0);
+            rewind($handle);
+
+            fwrite($handle,serialize($newData));
+            fclose($handle);
+
+        }
+
+    }
+
+?>

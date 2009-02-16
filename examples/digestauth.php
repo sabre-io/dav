459126ee28bdc9ac482082c3d87fea8efb37c96b
Evert Pot <evert@rooftopsolutions.nl>
2009-02-16 21:57:18 +0000

    added digest authentication example

---
+++ examples/digestauth.php
@@ -0,0 +1,29 @@
+<?php
+
+// !!!! Make sure the Sabre directory is in the include_path !!!
+
+// {$Id$} //
+
+// settings
+date_default_timezone_set('Canada/Eastern');
+
+// Files we need
+require_once 'Sabre.includes.php';
+
+$u = 'admin';
+$p = '1234';
+
+$auth = new Sabre_HTTP_DigestAuth();
+$auth->init();
+
+$result = $auth->getUserPass();
+
+if ($auth->getUsername() != $u || !$auth->validatePassword($p)) { 
+
+    $auth->requireLogin();
+    echo "Authentication required\n";
+    die();
+
+}
+
+?>

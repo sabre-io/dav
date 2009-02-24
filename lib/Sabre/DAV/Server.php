3e782637c6c8f8b43fc87efc5f499e50485778a5
Evert Pot <evert@rooftopsolutions.nl>
2009-02-24 05:27:34 +0000

    Also returning supportedlock and lockdiscovery when an 'allprops' request is made

--- lib/Sabre/DAV/Server.php
+++ lib/Sabre/DAV/Server.php
@@ -319,10 +319,10 @@ class Sabre_DAV_Server {
             if (isset($file['contenttype'])) $newProps['{DAV:}getcontenttype'] = $file['contenttype'];
             $newProps['href'] = $file['name']; 
 
-            if (in_array('{DAV:}supportedlock',$properties)) $newProps['{DAV:}supportedlock'] = new Sabre_DAV_Property_SupportedLock($this->tree->supportsLocks());
+            if (!$properties || in_array('{DAV:}supportedlock',$properties)) $newProps['{DAV:}supportedlock'] = new Sabre_DAV_Property_SupportedLock($this->tree->supportsLocks());
             
             if ($this->tree->supportsLocks()) 
-                if (in_array('{DAV:}lockdiscovery',$properties)) $newProps['{DAV:}lockdiscovery'] = new Sabre_DAV_Property_LockDiscovery($this->tree->getLocks($path));
+                if (!$properties || in_array('{DAV:}lockdiscovery',$properties)) $newProps['{DAV:}lockdiscovery'] = new Sabre_DAV_Property_LockDiscovery($this->tree->getLocks($path));
              
 
             //print_r($newProps);die();
@@ -1113,7 +1113,7 @@ class Sabre_DAV_Server {
 
             $propName = null;
             preg_match('/^{([^}]*)}(.*)$/',$property,$propName);
-         
+        
             // special case for empty namespaces
             if ($propName[1]=='') {
 

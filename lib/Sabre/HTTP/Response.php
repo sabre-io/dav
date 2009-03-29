464539454dce516b67295e0bb2e29dc600ffbcce
Evert Pot <evert@rooftopsolutions.nl>
2009-03-29 18:01:26 +0000

    Complete HTTP status code list (for completeness, even though they might not be used)

--- lib/Sabre/HTTP/Response.php
+++ lib/Sabre/HTTP/Response.php
@@ -21,25 +21,51 @@ class Sabre_HTTP_Response {
     public function getStatusMessage($code) {
 
         $msg = array(
+            100 => 'Continue',
+            101 => 'Switching Protocols',
             200 => 'Ok',
             201 => 'Created',
+            202 => 'Accepted',
+            203 => 'Non-Authorative Information',
             204 => 'No Content',
+            205 => 'Reset Content',
             206 => 'Partial Content',
-            207 => 'Multi-Status',
+            207 => 'Multi-Status', // RFC 4918
+            300 => 'Multiple Choices',
+            301 => 'Moved Permanently',
+            302 => 'Found',
+            303 => 'See Other',
+            304 => 'Not Modified',
+            305 => 'Use Proxy',
+            307 => 'Temporary Redirect',
             400 => 'Bad request',
             401 => 'Unauthorized',
+            402 => 'Payment Required',
             403 => 'Forbidden',
             404 => 'Not Found',
-            405 => 'Method not allowed',
+            405 => 'Method Not Allowed',
+            406 => 'Not Acceptable',
+            407 => 'Proxy Authentication Required',
+            408 => 'Request Timeout',
             409 => 'Conflict',
+            410 => 'Gone',
+            411 => 'Length Required',
             412 => 'Precondition failed',
+            413 => 'Request Entity Too Large',
             414 => 'Request-URI Too Long',
             415 => 'Unsupported Media Type',
             416 => 'Requested Range Not Satisfiable',
-            423 => 'Locked',
+            417 => 'Expectation Failed',
+            422 => 'Unprocessable Entity', // RFC 4918
+            423 => 'Locked', // RFC 4918
+            424 => 'Failed Dependency', // RFC 4918
             500 => 'Internal Server Error',
             501 => 'Not Implemented',
-            507 => 'Unsufficient Storage',
+            502 => 'Bad Gateway',
+            503 => 'Service Unavailable',
+            504 => 'Gateway Timeout',
+            505 => 'HTTP Version not supported',
+            507 => 'Unsufficient Storage', // RFC 4918
        ); 
 
        return 'HTTP/1.1 ' . $code . ' ' . $msg[$code];

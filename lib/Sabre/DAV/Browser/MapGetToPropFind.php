09e40be1e3212f2749856efe1e0ea308df19d31c
Evert Pot <evert@rooftopsolutions.nl>
2009-07-28 23:44:33 +0000

    Added missing svn:keyword Id

--- lib/Sabre/DAV/Browser/MapGetToPropFind.php
+++ lib/Sabre/DAV/Browser/MapGetToPropFind.php
@@ -8,7 +8,7 @@
  * 
  * @package Sabre
  * @subpackage DAV
- * @version $Id: Plugin.php 456 2009-07-12 21:40:30Z evertpot $
+ * @version $Id$
  * @copyright Copyright (C) 2007-2009 Rooftop Solutions. All rights reserved.
  * @author Evert Pot (http://www.rooftopsolutions.nl/)
  * @license http://code.google.com/p/sabredav/wiki/License Modified BSD License

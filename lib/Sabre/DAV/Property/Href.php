1ccf33e7695f86228548d55eb230c9e7e4bce71e
Evert Pot <evert@rooftopsolutions.nl>
2009-10-31 18:48:04 +0000

    Docblocks

--- lib/Sabre/DAV/Property/Href.php
+++ lib/Sabre/DAV/Property/Href.php
@@ -1,22 +1,60 @@
 <?php
 
+/**
+ * Href property
+ *
+ * The href property represpents a url within a {DAV:}href element.
+ * This is used by many WebDAV extensions, but not really within the WebDAV core spec
+ * 
+ * @package Sabre
+ * @subpackage DAV
+ * @version $Id$
+ * @copyright Copyright (C) 2007-2009 Rooftop Solutions. All rights reserved.
+ * @author Evert Pot (http://www.rooftopsolutions.nl/) 
+ * @license http://code.google.com/p/sabredav/wiki/License Modified BSD License
+ */
 class Sabre_DAV_Property_Href extends Sabre_DAV_Property  {
 
+    /**
+     * href 
+     * 
+     * @var string 
+     */
     private $href;
 
-    function __construct($href) {
+    /**
+     * __construct 
+     * 
+     * @param string $href 
+     * @return void
+     */
+    public function __construct($href) {
 
         $this->href = $href;
 
     }
 
+    /**
+     * Returns the uri 
+     * 
+     * @return string 
+     */
     public function getHref() {
 
         return $this->href;
 
     }
 
-    function serialize(Sabre_DAV_Server $server,DOMElement $dom) {
+    /**
+     * Serializes this property.
+     *
+     * It will additionally prepend the href property with the server's base uri.
+     * 
+     * @param Sabre_DAV_Server $server 
+     * @param DOMElement $dom 
+     * @return void
+     */
+    public function serialize(Sabre_DAV_Server $server,DOMElement $dom) {
 
         $elem = $dom->ownerDocument->createElementNS('DAV:','d:href');
         $elem->nodeValue = $server->getBaseUri() . $this->href;

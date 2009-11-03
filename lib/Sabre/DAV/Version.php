afc53010fd053f8e487b8ec23421bd09cf2a2c99
Evert Pot <evert@rooftopsolutions.nl>
2009-11-03 13:27:12 +0000

    Updated changelog, and pushed version to 1.0.1

--- lib/Sabre/DAV/Version.php
+++ lib/Sabre/DAV/Version.php
@@ -15,7 +15,7 @@ class Sabre_DAV_Version {
     /**
      * Full version number
      */
-    const VERSION = '1.0.0';
+    const VERSION = '1.0.1';
 
     /**
      * Stability : alpha, beta, stable

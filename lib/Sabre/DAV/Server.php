d56d5c696f39f0421658dcbd0f49c4ba8eab8bde
Evert Pot <evert@rooftopsolutions.nl>
2010-02-23 08:38:30 +0000

    Throwing 501 for HEAD on non-files
    Update issue #19
    Fixed issue on trunk

--- lib/Sabre/DAV/Server.php
+++ lib/Sabre/DAV/Server.php
@@ -370,7 +370,10 @@ class Sabre_DAV_Server {
     protected function httpHead() {
 
         $node = $this->tree->getNodeForPath($this->getRequestUri());
-        if ($node instanceof Sabre_DAV_IFile && $size = $node->getSize())
+
+        if (!($node instanceof Sabre_DAV_IFile)) throw new Sabre_DAV_Exception_NotImplemented('HEAD is only implemented on File objects');
+
+        if ($size = $node->getSize())
             $this->httpResponse->setHeader('Content-Length',$size);
 
         if ($etag = $node->getETag()) {

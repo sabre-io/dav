3eb93994507b618e3b448f444cfd27c43b4c0ba7
Evert Pot <evert@rooftopsolutions.nl>
2010-03-11 01:28:52 +0900

    Fixed some package names

--- lib/Sabre/CalDAV/Property/SupportedCalendarData.php
+++ lib/Sabre/CalDAV/Property/SupportedCalendarData.php
@@ -4,6 +4,7 @@
  * Sabre_CalDAV_Property_SupportedCalendarData
  *
  * @package Sabre
+ * @subpackage CalDAV
  * @copyright Copyright (C) 2007-2010 Rooftop Solutions. All rights reserved.
  * @author Evert Pot (http://www.rooftopsolutions.nl/) 
  * @license http://code.google.com/p/sabredav/wiki/License Modified BSD License

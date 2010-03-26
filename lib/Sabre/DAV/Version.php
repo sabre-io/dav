09238800ee77c4e4d84330996c91589507555fe7
Evert Pot <evert@rooftopsolutions.nl>
2010-03-26 13:14:22 +0900

    Switching to new versioning scheme

--- lib/Sabre/DAV/Version.php
+++ lib/Sabre/DAV/Version.php
@@ -14,7 +14,7 @@ class Sabre_DAV_Version {
     /**
      * Full version number
      */
-    const VERSION = '1.1.3';
+    const VERSION = '1.2.0alpha1';
 
     /**
      * Stability : alpha, beta, stable

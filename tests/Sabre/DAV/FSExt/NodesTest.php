3c6f3af32e5405ebcb3a7f180d5e909f401582d7
Evert Pot <evert@rooftopsolutions.nl>
2010-03-30 13:18:42 +0900

    Updated tests

--- tests/Sabre/DAV/FSExt/NodesTest.php
+++ tests/Sabre/DAV/FSExt/NodesTest.php
@@ -209,7 +209,7 @@ class Sabre_DAV_FSExt_NodesTest extends Sabre_DAV_AbstractServer{
             'Allow'          => 'OPTIONS, GET, HEAD, DELETE, TRACE, PROPFIND, MKCOL, PUT, PROPPATCH, COPY, MOVE, REPORT',
             'Accept-Ranges'  => 'bytes',
             'Content-Length' => '0',
-            'X-Sabre-Version' => Sabre_DAV_Version::VERSION . '-' . Sabre_DAV_Version::STABILITY,
+            'X-Sabre-Version' => Sabre_DAV_Version::VERSION,
         ),$this->response->headers);
 
         $this->assertEquals('HTTP/1.1 200 Ok',$this->response->status);

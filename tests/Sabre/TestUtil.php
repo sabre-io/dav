1ea72a7780e096f5193bd30e320e23cfc7964b1b
Evert Pot <evert@rooftopsolutions.nl>
2010-04-22 16:18:18 +0900

    Returning false if mysql is unavailable

--- tests/Sabre/TestUtil.php
+++ tests/Sabre/TestUtil.php
@@ -35,10 +35,13 @@ class Sabre_TestUtil {
 
     static function getMySQLDB() {
 
-        $pdo = new PDO(SABRE_MYSQLDSN,SABRE_MYSQLUSER,SABRE_MYSQLPASS);
-        $pdo->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
-
-        return $pdo;
+        try {
+            $pdo = new PDO(SABRE_MYSQLDSN,SABRE_MYSQLUSER,SABRE_MYSQLPASS);
+            $pdo->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
+            return $pdo;
+        } catch (PDOException $e) {
+            return null;
+        }
     
     }
 

cda30b32576e096abda961d813a156215fa15d19
Evert Pot <evert@rooftopsolutions.nl>
2010-04-23 19:49:56 +0900

    New autoloader

--- lib/Sabre.autoload.php
+++ lib/Sabre.autoload.php
@@ -3,27 +3,14 @@
 /**
  * SabreDAV's PHP autoloader
  *
- * If you love the autoloader, and don't care as much about performance, this
- * file register a new autoload function using spl_autoload_register. 
+ * Warning: this file is deprecated. Please use Sabre/autoload.php instead.
  *
  * @package Sabre
  * @subpackage DAV
- * @version $Id$
  * @copyright Copyright (C) 2007-2010 Rooftop Solutions. All rights reserved.
  * @author Evert Pot (http://www.rooftopsolutions.nl/) 
+ * @deprecated
  * @license http://code.google.com/p/sabredav/wiki/License Modified BSD License
  */
 
-function Sabre_DAV_autoload($className) {
-
-    if(strpos($className,'Sabre_')===0) {
-
-        include dirname(__FILE__) . '/' . str_replace('_','/',$className) . '.php';
-
-    }
-
-}
-
-spl_autoload_register('Sabre_DAV_autoload');
-
-?>
+include dirname(__FILE__) . '/Sabre/autoload.php';

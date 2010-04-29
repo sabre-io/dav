95680a98b5c98149c1db87f71e95cfc936ebb904
Evert Pot <evert@rooftopsolutions.nl>
2010-04-29 12:10:51 +0900

    Adding email address to example scripts

--- examples/sql/sqlite.users.sql
+++ examples/sql/sqlite.users.sql
@@ -6,5 +6,5 @@ CREATE TABLE users (
 	UNIQUE(username)
 );
 
-INSERT INTO users (username,digesta1) VALUES
-('admin',  '87fd274b7b6c01e48d7c2f965da8ddf7');
+INSERT INTO users (username,digesta1,email) VALUES
+('admin',  '87fd274b7b6c01e48d7c2f965da8ddf7','admin@example.org');

ab4d2b813340ada527fddc0a7304836bc280d65d
Evert Pot <evert@rooftopsolutions.nl>
2010-06-18 09:16:15 +0200

    Default value for ctag, in case it had been set to null in the database

--- lib/Sabre/CalDAV/Backend/PDO.php
+++ lib/Sabre/CalDAV/Backend/PDO.php
@@ -76,7 +76,7 @@ class Sabre_CalDAV_Backend_PDO extends Sabre_CalDAV_Backend_Abstract {
                 'id' => $row['id'],
                 'uri' => $row['uri'],
                 'principaluri' => $row['principaluri'],
-                '{' . Sabre_CalDAV_Plugin::NS_CALENDARSERVER . '}getctag' => $row['ctag'],
+                '{' . Sabre_CalDAV_Plugin::NS_CALENDARSERVER . '}getctag' => $row['ctag']?$row['ctag']:'0',
                 '{' . Sabre_CalDAV_Plugin::NS_CALDAV . '}supported-calendar-component-set' => new Sabre_CalDAV_Property_SupportedCalendarComponentSet($components),
             );
         

660f2e5bc2d224e67f9572fcb51d37aad8683017
Evert Pot <evert@rooftopsolutions.nl>
2010-12-12 18:53:51 +0100

    More unittests. Back at 96% for Sabre_DAV

--- tests/Sabre/DAVACL/SimplePluginTest.php
+++ tests/Sabre/DAVACL/SimplePluginTest.php
@@ -8,6 +8,14 @@ class Sabre_DAVACL_SimplePluginTest extends PHPUnit_Framework_TestCase {
         $this->assertEquals('acl',$aclPlugin->getPluginName());
         $this->assertEquals(array('access-control'), $aclPlugin->getFeatures());
 
+        $this->assertEquals(
+            array(
+                '{DAV:}expand-properties',
+                '{DAV:}principal-property-search',
+                '{DAV:}principal-search-property-set'
+            ), 
+            $aclPlugin->getSupportedReportSet(''));
+
     }
 
 }

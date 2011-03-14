356ef02a49f58a0fa6643e2fd6d41e708879f507
Evert Pot <evert@rooftopsolutions.nl>
2011-03-14 23:32:06 +0100

    Bumped versions

--- lib/Sabre/DAVACL/Version.php
+++ lib/Sabre/DAVACL/Version.php
@@ -14,7 +14,7 @@ class Sabre_DAVACL_Version {
     /**
      * Full version number
      */
-    const VERSION = '1.4.0';
+    const VERSION = '1.4.2';
 
     /**
      * Stability : alpha, beta, stable

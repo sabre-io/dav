05f8d0aedb354626c080914da5b8bc60da742f94
Evert Pot <evert@rooftopsolutions.nl>
2011-04-26 21:45:13 +0200

    Fixed docblocks

--- lib/Sabre/VObject/Property.php
+++ lib/Sabre/VObject/Property.php
@@ -12,6 +12,7 @@
  * Parameters can be accessed using the ArrayAccess interface. 
  *
  * @package Sabre
+ * @subpackage VObject
  * @copyright Copyright (C) 2007-2011 Rooftop Solutions. All rights reserved.
  * @author Evert Pot (http://www.rooftopsolutions.nl/) 
  * @license http://code.google.com/p/sabredav/wiki/License Modified BSD License

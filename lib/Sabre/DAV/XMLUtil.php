faf393e6f856110d585b1efb06d413145158b87b
Evert Pot <evert@rooftopsolutions.nl>
2011-07-11 12:24:13 +1000

    Added clark-notation parser

--- lib/Sabre/DAV/XMLUtil.php
+++ lib/Sabre/DAV/XMLUtil.php
@@ -42,6 +42,29 @@ class Sabre_DAV_XMLUtil {
     }
 
     /**
+     * Parses a clark-notation string, and returns the namespace and element 
+     * name components.
+     *
+     * If the string was invalid, it will throw an InvalidArgumentException.
+     * 
+     * @param string $str
+     * @throws InvalidArgumentException 
+     * @return array 
+     */
+    static function parseClarkNotation($str) {
+
+        if (!preg_match('/^{([^}]*)}(.*)$/',$str,$matches)) {
+            throw new InvalidArgumentException('\'' . $str . '\' is not a valid clark-notation formatted string');
+        }
+
+        return array(
+            $matches[1],
+            $matches[2]
+        );
+
+    }
+
+    /**
      * This method takes an XML document (as string) and converts all instances of the
      * DAV: namespace to urn:DAV
      *

fe8a7bc445c2f172b358498af386865294b1efcf
Evert Pot <evert@rooftopsolutions.nl>
2011-07-15 09:48:13 +1000

    Added unittest

--- tests/Sabre/CardDAV/Backend/AbstractPDOTest.php
+++ tests/Sabre/CardDAV/Backend/AbstractPDOTest.php
@@ -32,5 +32,84 @@ abstract class Sabre_CardDAV_Backend_AbstractPDOTest extends PHPUnit_Framework_T
 
     }
 
+    public function testUpdateAddressBookInvalidProp() {
+
+        $result = $this->backend->updateAddressBook(1, array(
+            '{DAV:}displayname' => 'updated',
+            '{' . Sabre_CardDAV_Plugin::NS_CARDDAV . '}addressbook-description' => 'updated',
+            '{DAV:}foo' => 'bar',
+        ));
+
+        $this->assertFalse($result);
+
+        $result = $this->backend->getAddressBooksForUser('principals/user1');
+
+        $expected = array(
+            array(
+                'id' => 1,
+                'uri' => 'book1',
+                'principaluri' => 'principals/user1',
+                '{DAV:}displayname' => 'book1',
+                '{' . Sabre_CardDAV_Plugin::NS_CARDDAV . '}addressbook-description' => 'addressbook 1',
+                '{http://calendarserver.org/ns/}getctag' => 1,
+            )
+        );
+
+        $this->assertEquals($expected, $result);
+        
+
+    }
+
+    public function testUpdateAddressBookNoProps() {
+
+        $result = $this->backend->updateAddressBook(1, array());
+
+        $this->assertFalse($result);
+
+        $result = $this->backend->getAddressBooksForUser('principals/user1');
+
+        $expected = array(
+            array(
+                'id' => 1,
+                'uri' => 'book1',
+                'principaluri' => 'principals/user1',
+                '{DAV:}displayname' => 'book1',
+                '{' . Sabre_CardDAV_Plugin::NS_CARDDAV . '}addressbook-description' => 'addressbook 1',
+                '{http://calendarserver.org/ns/}getctag' => 1,
+            )
+        );
+
+        $this->assertEquals($expected, $result);
+        
+
+    }
+
+    public function testUpdateAddressBookSuccess() {
+
+        $result = $this->backend->updateAddressBook(1, array(
+            '{DAV:}displayname' => 'updated',
+            '{' . Sabre_CardDAV_Plugin::NS_CARDDAV . '}addressbook-description' => 'updated',
+        ));
+
+        $this->assertTrue($result);
+
+        $result = $this->backend->getAddressBooksForUser('principals/user1');
+
+        $expected = array(
+            array(
+                'id' => 1,
+                'uri' => 'book1',
+                'principaluri' => 'principals/user1',
+                '{DAV:}displayname' => 'updated',
+                '{' . Sabre_CardDAV_Plugin::NS_CARDDAV . '}addressbook-description' => 'updated',
+                '{http://calendarserver.org/ns/}getctag' => 2,
+            )
+        );
+
+        $this->assertEquals($expected, $result);
+        
+
+    }
+
 }
 

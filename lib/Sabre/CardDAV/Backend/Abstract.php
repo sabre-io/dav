e306f8e0a408dcc69da572aba8eaeb89d80a3de5
Evert Pot <evert@rooftopsolutions.nl>
2011-07-19 17:02:54 +1000

    Docblocks

--- lib/Sabre/CardDAV/Backend/Abstract.php
+++ lib/Sabre/CardDAV/Backend/Abstract.php
@@ -20,7 +20,18 @@
 abstract class Sabre_CardDAV_Backend_Abstract {
 
     /**
-     * Returns the list of addressbooks for a specific user. 
+     * Returns the list of addressbooks for a specific user.
+     *
+     * Every addressbook should have the following properties:
+     *   id - an arbitrary unique id
+     *   uri - the 'basename' part of the url
+     *   principaluri - Same as the passed parameter
+     *
+     * Any additional clark-notation property may be passed besides this. Some 
+     * common ones are :
+     *   {DAV:}displayname
+     *   {urn:ietf:params:xml:ns:carddav}addressbook-description
+     *   {http://calendarserver.org/ns/}getctag
      * 
      * @param string $principalUri 
      * @return array 
@@ -53,14 +64,19 @@ abstract class Sabre_CardDAV_Backend_Abstract {
     /**
      * Deletes an entire addressbook and all its contents
      *
-     * @param int $addressBookId 
+     * @param mixed $addressBookId 
      * @return void
      */
     abstract public function deleteAddressBook($addressBookId); 
 
     /**
      * Returns all cards for a specific addressbook id. 
-     * 
+     *
+     * This method should return the following properties for each card:
+     *   * carddata - raw vcard data
+     *   * uri - Some unique url
+     *   * lastmodified - A unix timestamp
+
      * @param mixed $addressbookId 
      * @return array 
      */

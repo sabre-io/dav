61b4ba213bc1d623e104a53ccf5a6c5d8c2749f7
Evert Pot <evert@rooftopsolutions.nl>
2011-08-20 17:49:21 -0400

    Updated changelog + bumped version

--- lib/Sabre/CardDAV/Version.php
+++ lib/Sabre/CardDAV/Version.php
@@ -18,7 +18,7 @@ class Sabre_CardDAV_Version {
     /**
      * Full version number
      */
-    const VERSION = '1.5.0';
+    const VERSION = '1.5.1';
 
     /**
      * Stability : alpha, beta, stable

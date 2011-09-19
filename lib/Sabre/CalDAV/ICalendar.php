e02ec3f316564c4bb7495ab27c47a879d69fa8c9
Evert Pot <evert@rooftopsolutions.nl>
2011-09-19 23:56:39 +0100

    Fixed broken docblocks. Updates Issue #151.

--- lib/Sabre/CalDAV/ICalendar.php
+++ lib/Sabre/CalDAV/ICalendar.php
@@ -6,7 +6,7 @@
  * Implement this interface to allow a node to be recognized as an calendar.
  * 
  * @package Sabre
- * @subpackage CardDAV
+ * @subpackage CalDAV
  * @copyright Copyright (C) 2007-2011 Rooftop Solutions. All rights reserved.
  * @author Evert Pot (http://www.rooftopsolutions.nl/) 
  * @license http://code.google.com/p/sabredav/wiki/License Modified BSD License
--- lib/Sabre/CalDAV/ICalendarObject.php
+++ lib/Sabre/CalDAV/ICalendarObject.php
@@ -1,19 +1,19 @@
 <?php
 
 /**
- * Event interface 
+ * CalendarObject interface 
+/**
+ * Extend the ICalendarObject interface to allow your custom nodes to be picked up as 
+ * CalendarObjects.
+ *
+ * Calendar objects are resources such as Events, Todo's or Journals.
  * 
  * @package Sabre
- * @subpackage CardDAV
+ * @subpackage CalDAV
  * @copyright Copyright (C) 2007-2011 Rooftop Solutions. All rights reserved.
  * @author Evert Pot (http://www.rooftopsolutions.nl/) 
  * @license http://code.google.com/p/sabredav/wiki/License Modified BSD License
- *
- /
-/**
- * Extend the ICalendarObject interface to allow your custom nodes to be picked up as 
- * 'Events'. 
- */ 
+ */
 interface Sabre_CalDAV_ICalendarObject extends Sabre_DAV_IFile { 
 
 }

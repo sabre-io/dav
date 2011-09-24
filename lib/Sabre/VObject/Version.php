81338ea723664daa21852fabd92e518ce38f113a
Evert Pot <evert@rooftopsolutions.nl>
2011-09-24 20:32:29 +0100

    Support for mixed newlines. Fixes Issue 153.

--- lib/Sabre/VObject/Version.php
+++ lib/Sabre/VObject/Version.php
@@ -14,7 +14,7 @@ class Sabre_VObject_Version {
     /**
      * Full version number
      */
-    const VERSION = '1.2.1';
+    const VERSION = '1.2.2';
 
     /**
      * Stability : alpha, beta, stable

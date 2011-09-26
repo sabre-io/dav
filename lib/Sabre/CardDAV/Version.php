fafae643c48a8a96e1cd1a2c1ac41ac934ec6afb
Evert Pot <evert@rooftopsolutions.nl>
2011-09-26 14:06:45 +0100

    Support for CARDDAV:supported-address-data. Fixes Issue 158.

--- lib/Sabre/CardDAV/Version.php
+++ lib/Sabre/CardDAV/Version.php
@@ -18,7 +18,7 @@ class Sabre_CardDAV_Version {
     /**
      * Full version number
      */
-    const VERSION = '1.5.2';
+    const VERSION = '1.5.3';
 
     /**
      * Stability : alpha, beta, stable

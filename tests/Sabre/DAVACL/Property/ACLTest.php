5ee9f45ca2eefd84b74ca9d575c875de8a5974f6
Evert Pot <evert@rooftopsolutions.nl>
2011-10-10 18:51:03 +0100

    Added support for authenticated, unauthenticated and all 'principal types'

--- tests/Sabre/DAVACL/Property/ACLTest.php
+++ tests/Sabre/DAVACL/Property/ACLTest.php
@@ -155,10 +155,7 @@ class Sabre_DAVACL_Property_ACLTest extends PHPUnit_Framework_TestCase {
 
     }
 
-    /**
-     * @expectedException Sabre_DAV_Exception_NotImplemented
-     */
-    function testUnserializeUnsupportedPrincipal() {
+    function testUnserializeOtherPrincipal() {
 
         $source = '<?xml version="1.0"?>
 <d:root xmlns:d="DAV:">
@@ -170,11 +167,50 @@ class Sabre_DAVACL_Property_ACLTest extends PHPUnit_Framework_TestCase {
     </d:grant>
     <d:principal><d:authenticated /></d:principal>  
   </d:ace>
+  <d:ace>
+    <d:grant>
+      <d:privilege>
+        <d:write/>
+      </d:privilege>
+    </d:grant>
+    <d:principal><d:unauthenticated /></d:principal>  
+  </d:ace>
+  <d:ace>
+    <d:grant>
+      <d:privilege>
+        <d:write/>
+      </d:privilege>
+    </d:grant>
+    <d:principal><d:all /></d:principal>  
+  </d:ace>
 </d:root>
 ';
 
         $dom = Sabre_DAV_XMLUtil::loadDOMDocument($source);
-        Sabre_DAVACL_Property_Acl::unserialize($dom->firstChild);
+        $result = Sabre_DAVACL_Property_Acl::unserialize($dom->firstChild);
+
+        $this->assertInstanceOf('Sabre_DAVACL_Property_Acl', $result);
+
+        $expected = array(
+            array(
+                'principal' => '{DAV:}authenticated',
+                'protected' => false,
+                'privilege' => '{DAV:}write',
+            ),
+            array(
+                'principal' => '{DAV:}unauthenticated',
+                'protected' => false,
+                'privilege' => '{DAV:}write',
+            ),
+            array(
+                'principal' => '{DAV:}all',
+                'protected' => false,
+                'privilege' => '{DAV:}write',
+            ),
+        );
+
+        $this->assertEquals($expected, $result->getPrivileges());
+
 
     }
 

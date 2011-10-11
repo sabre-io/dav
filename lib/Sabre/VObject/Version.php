1ffb42b8db062a318542e902a117798ab00c654a
Evert Pot <evert@rooftopsolutions.nl>
2011-10-11 20:28:01 +0100

    Bumped version, updated changelog

--- lib/Sabre/VObject/Version.php
+++ lib/Sabre/VObject/Version.php
@@ -14,7 +14,7 @@ class Sabre_VObject_Version {
     /**
      * Full version number
      */
-    const VERSION = '1.2.2';
+    const VERSION = '1.2.3';
 
     /**
      * Stability : alpha, beta, stable

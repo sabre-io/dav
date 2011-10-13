6aaa58abe642e1f2b3745fabfed4723fd93f315a
Evert Pot <evert@rooftopsolutions.nl>
2011-10-13 14:30:09 +0100

    Added a few more tests

--- tests/Sabre/CalDAV/Schedule/PluginTest.php
+++ tests/Sabre/CalDAV/Schedule/PluginTest.php
@@ -1,5 +1,7 @@
 <?php
 
+require_once 'Sabre/DAVACL/MockPrincipalBackend.php';
+
 class Sabre_CalDAV_Schedule_PluginTest extends PHPUnit_Framework_TestCase {
 
     /**
@@ -29,6 +31,45 @@ class Sabre_CalDAV_Schedule_PluginTest extends PHPUnit_Framework_TestCase {
         $this->assertEquals(array('calendar-auto-schedule'), $plugin->getFeatures());
 
     }
+
+    public function testPrincipalProperties() {
+
+        $principalBackend = new Sabre_DAVACL_MockPrincipalBackend();
+
+        $tree = array(
+            new Sabre_DAVACL_PrincipalCollection($principalBackend),
+        );
+
+        $server = new Sabre_DAV_Server($tree);
+
+        $acl = new Sabre_DAVACL_Plugin();
+        $server->addPlugin($acl);
+
+        $plugin = new Sabre_CalDAV_Schedule_Plugin();
+        $server->addPlugin($plugin);
+
+        $result = $server->getPropertiesForChildren('principals',array(
+            '{' . Sabre_CalDAV_Plugin::NS_CALDAV . '}schedule-inbox-URL',
+            '{' . Sabre_CalDAV_Plugin::NS_CALDAV . '}schedule-outbox-URL',
+        ));
+
+        $expected = array(
+            'principals/admin/' => array(
+                '{' . Sabre_CalDAV_Plugin::NS_CALDAV . '}schedule-inbox-URL' => new Sabre_DAV_Property_Href('schedule/admin/inbox'),
+                '{' . Sabre_CalDAV_Plugin::NS_CALDAV . '}schedule-outbox-URL' => new Sabre_DAV_Property_Href('schedule/admin/outbox'),
+            ),
+            'principals/user1/' => array(
+                '{' . Sabre_CalDAV_Plugin::NS_CALDAV . '}schedule-inbox-URL' => new Sabre_DAV_Property_Href('schedule/user1/inbox'),
+                '{' . Sabre_CalDAV_Plugin::NS_CALDAV . '}schedule-outbox-URL' => new Sabre_DAV_Property_Href('schedule/user1/outbox'),
+            ),
+            'principals/user2/' => array(
+                '{' . Sabre_CalDAV_Plugin::NS_CALDAV . '}schedule-inbox-URL' => new Sabre_DAV_Property_Href('schedule/user2/inbox'),
+                '{' . Sabre_CalDAV_Plugin::NS_CALDAV . '}schedule-outbox-URL' => new Sabre_DAV_Property_Href('schedule/user2/outbox'),
+            ),
+        );
+        $this->assertEquals($expected,$result);
+
+    } 
 
 
 }

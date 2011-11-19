48b0ae806cc00f326ae6a9dc790cd425714e4616
Evert Pot <evert@rooftopsolutions.nl>
2011-11-19 14:33:35 +0100

    Brought the debugplugin branch up to date, and added a few improvements

--- lib/Sabre/DAV/DebugPlugin.php
+++ lib/Sabre/DAV/DebugPlugin.php
@@ -7,18 +7,19 @@ class Sabre_DAV_DebugPlugin extends Sabre_DAV_ServerPlugin {
 
     protected $startTime;
 
+    protected $logLevel = 1;
+
     protected $contentTypeWhiteList = array(
         '|^text/|',
         '|^application/xml|',
     );
 
-    function __construct($logFile) {
+    function __construct($logFile, $logLevel = 1) {
 
-        if (!is_writeable($logFile)) {
-            throw new Sabre_DAV_Exception('Unable to write to: ' . $logFile);
-        }
+        $logFile = str_replace('%t',time(), $logFile);
 
         $this->logHandle = fopen($logFile,'a');
+        $this->logLevel = $logLevel;
         $this->startTime = time();
 
     }
@@ -42,7 +43,7 @@ class Sabre_DAV_DebugPlugin extends Sabre_DAV_ServerPlugin {
         $this->server->subscribeEvent('unknownMethod', array($this, 'unknownMethod'), 5);
         $this->server->subscribeEvent('report', array($this, 'report'), 5);
         $this->server->subscribeEvent('beforeGetProperties', array($this, 'beforeGetProperties'), 5);
-        $this->log('Initialized plugin. Request time ' . $this->startTime . ' (' . date(DateTime::RFC2822,$this->startTime) . '). Version: ' . Sabre_DAV_Version::VERSION);
+        $this->log(2,'Initialized plugin. Request time ' . $this->startTime . ' (' . date(DateTime::RFC2822,$this->startTime) . '). Version: ' . Sabre_DAV_Version::VERSION);
 
     }
 
@@ -56,15 +57,17 @@ class Sabre_DAV_DebugPlugin extends Sabre_DAV_ServerPlugin {
      */
     public function beforeMethod($method, $uri) {
 
-        $this->log('beforeMethod triggered. Method: ' . $method . ' uri: ' . ($uri?$uri:'(root)'));
-        $this->log('Plugins loaded:');
+        $this->log(3,'beforeMethod triggered. Method: ' . $method . ' uri: ' . ($uri?$uri:'(root)'));
+        $this->log(2,'Plugins loaded:');
         foreach($this->server->getPlugins() as $pluginName => $plugin) {
-            $this->log('  ' . $pluginName . ' (' . get_class($plugin) . ')');
+            $this->log(2,'  ' . $pluginName . ' (' . get_class($plugin) . ')');
         }
-        $this->log('SabreDAV server Base URI: ' . $this->server->getBaseUri()); 
-        $this->log('HTTP Request:');
-        $this->log($this->server->httpRequest->getMethod() . ' ' . $this->server->httpRequest->getUri());
-        $this->log(print_r($this->server->httpRequest->getHeaders(),true));
+        $this->log(2,'SabreDAV server Base URI: ' . $this->server->getBaseUri()); 
+        $this->log(1,$this->server->httpRequest->getMethod() . ' ' . $this->server->httpRequest->getUri() . ' -> ' . $this->server->getRequestUri());
+        $this->log(2,'Headers:');
+        foreach($this->server->httpRequest->getHeaders() as $key=>$value) {
+            $this->log(2,'  '  . $key . ': ' . $value);
+        }
 
         // We're only going to show the request body if it's text-based. The 
         // maximum size will be 10k.
@@ -78,11 +81,7 @@ class Sabre_DAV_DebugPlugin extends Sabre_DAV_ServerPlugin {
             }
 
         }
-        if (!$showBody) {
-            $this->log('Request body Content-Type does not match our whitelist, so it is not displayed here');
-            $this->log(print_r($_SERVER,true));
-        } else {
-
+        if ($showBody && $this->logLevel>=2) {
             // We need to grab the body, and put it in an intermediate stream.
             $newBody = fopen('php://temp','r+');
             $body = $this->server->httpRequest->getBody();
@@ -117,7 +116,7 @@ class Sabre_DAV_DebugPlugin extends Sabre_DAV_ServerPlugin {
      */
     public function unknownMethod($method, $uri) {
 
-        $this->log('unknownMethod triggered. Method: ' . $method . ' uri: ' . ($uri?$uri:'(root)'));
+        $this->log(3,'unknownMethod triggered. Method: ' . $method . ' uri: ' . ($uri?$uri:'(root)'));
 
     }
     /**
@@ -128,7 +127,7 @@ class Sabre_DAV_DebugPlugin extends Sabre_DAV_ServerPlugin {
      */
     public function report($reportName) {
 
-        $this->log('Report requested: ' . $reportName);
+        $this->log(3,'Report requested: ' . $reportName);
 
     }
 
@@ -141,8 +140,8 @@ class Sabre_DAV_DebugPlugin extends Sabre_DAV_ServerPlugin {
      */
     public function beforeGetProperties($path, Sabre_DAV_INode $node, $requestedProperties, $returnedProperties) {
 
-        $this->log('Properties requested for uri (' . $path . '):');
-        $this->log(print_r($requestedProperties,true));
+        $this->log(3,'Properties requested for uri (' . $path . '):');
+        $this->log(3,print_r($requestedProperties,true));
 
     }
 
@@ -152,9 +151,11 @@ class Sabre_DAV_DebugPlugin extends Sabre_DAV_ServerPlugin {
      * @param string $message 
      * @return void
      */
-    public function log($message) {
+    public function log($logLevel, $message) {
 
-        fwrite($this->logHandle,$message . "\n");
+        if ($logLevel <= $this->logLevel) {
+            fwrite($this->logHandle,$message . "\n");
+        }
 
     }
 

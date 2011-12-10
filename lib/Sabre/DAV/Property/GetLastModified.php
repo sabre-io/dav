559ff3bfb2afbf528b0e45fceaa1f7732eb99b30
Evert Pot <evert@rooftopsolutions.nl>
2011-12-10 02:44:47 +0100

    Using the GMT date everywhere it's appropriate

--- lib/Sabre/DAV/Property/GetLastModified.php
+++ lib/Sabre/DAV/Property/GetLastModified.php
@@ -56,7 +56,7 @@ class Sabre_DAV_Property_GetLastModified extends Sabre_DAV_Property {
         $doc = $prop->ownerDocument;
         $prop->setAttribute('xmlns:b','urn:uuid:c2f41010-65b3-11d1-a29f-00aa00c14882/');
         $prop->setAttribute('b:dt','dateTime.rfc1123');
-        $prop->nodeValue = $this->time->format(DateTime::RFC1123);
+        $prop->nodeValue = Sabre_HTTP_Util::toHTTPDate($this->time);
 
     }
 

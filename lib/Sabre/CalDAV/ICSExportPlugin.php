b00526bfb88afe2f41db8d0783bd5019494eac1d
Evert Pot <evert@rooftopsolutions.nl>
2012-01-24 14:44:24 +0100

    The ICS export plugin now properly checks the ACL

--- lib/Sabre/CalDAV/ICSExportPlugin.php
+++ lib/Sabre/CalDAV/ICSExportPlugin.php
@@ -55,9 +55,19 @@ class Sabre_CalDAV_ICSExportPlugin extends Sabre_DAV_ServerPlugin {
 
         if (!($node instanceof Sabre_CalDAV_Calendar)) return;
 
+        // Checking ACL, if available.
+        if ($aclPlugin = $this->server->getPlugin('acl')) {
+            $aclPlugin->checkPrivileges($uri, '{DAV:}read');
+        }
+
         $this->server->httpResponse->setHeader('Content-Type','text/calendar');
         $this->server->httpResponse->sendStatus(200);
-        $this->server->httpResponse->sendBody($this->generateICS($this->server->tree->getChildren($uri)));
+
+        $nodes = $this->server->getPropertiesForPath($uri, array(
+            '{' . Sabre_CalDAV_Plugin::NS_CALDAV . '}calendar-data',
+        ),1);
+
+        $this->server->httpResponse->sendBody($this->generateICS($nodes));
 
         // Returning false to break the event chain
         return false;
@@ -84,7 +94,11 @@ class Sabre_CalDAV_ICSExportPlugin extends Sabre_DAV_ServerPlugin {
 
         foreach($nodes as $node) {
 
-            $nodeData = $node->get();
+            if (!isset($node[200]['{' . Sabre_CalDAV_Plugin::NS_CALDAV . '}calendar-data'])) {
+                continue;
+            }
+            $nodeData = $node[200]['{' . Sabre_CalDAV_Plugin::NS_CALDAV . '}calendar-data'];
+
             $nodeComp = Sabre_VObject_Reader::read($nodeData);
 
             foreach($nodeComp->children() as $child) {
@@ -105,12 +119,9 @@ class Sabre_CalDAV_ICSExportPlugin extends Sabre_DAV_ServerPlugin {
                         $collectedTimezones[] = $child->TZID;
                         break;
 
-
                 }
 
-
             }
-
 
         }
 

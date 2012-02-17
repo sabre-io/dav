b054b1823de2a4c1186d96075564113b643b22d8
Evert Pot <evert@rooftopsolutions.nl>
2012-02-17 17:03:27 +0100

    Cleanups

--- tests/Sabre/DAVServerTest.php
+++ tests/Sabre/DAVServerTest.php
@@ -7,12 +7,12 @@ require_once 'Sabre/DAVACL/MockPrincipalBackend.php';
 /**
  * This class may be used as a basis for other webdav-related unittests.
  *
- * This class is supposed to provide a reasonably big framework to quickly get 
+ * This class is supposed to provide a reasonably big framework to quickly get
  * a testing environment running.
- * 
+ *
  * @package Sabre
  * @copyright Copyright (C) 2007-2012 Rooftop Solutions. All rights reserved.
- * @author Evert Pot (http://www.rooftopsolutions.nl/) 
+ * @author Evert Pot (http://www.rooftopsolutions.nl/)
  * @license http://code.google.com/p/sabredav/wiki/License Modified BSD License
  */
 abstract class Sabre_DAVServerTest extends PHPUnit_Framework_TestCase {
@@ -34,7 +34,7 @@ abstract class Sabre_DAVServerTest extends PHPUnit_Framework_TestCase {
 
         $this->setUpBackends();
         $this->setUpTree();
-        
+
         $this->server = new Sabre_DAV_Server($this->tree);
 
         if ($this->setupCalDAV) {
@@ -45,8 +45,30 @@ abstract class Sabre_DAVServerTest extends PHPUnit_Framework_TestCase {
 
     }
 
+    /**
+     * Makes a request, and returns a response object.
+     *
+     * You can either pass an isntance of Sabre_HTTP_Request, or an array,
+     * which will then be used as the _SERVER array.
+     *
+     * @param array|Sabre_HTTP_Request $request
+     * @return Sabre_HTTP_Response
+     */
+    function request($request) {
+
+        if (is_array($request)) {
+            $request = new Sabre_HTTP_Request($request);
+        }
+        $this->server->httpRequest = $request;
+        $this->server->httpResponse = new Sabre_HTTP_ResponseMock();
+        $this->server->exec();
+
+        return $this->server->httpResponse;
+
+    }
+
     function setUpTree() {
-        
+
         $this->tree = array();
         if ($this->setupCalDAV) {
             $this->tree[] = new Sabre_CalDAV_CalendarRootNode(
@@ -69,15 +91,6 @@ abstract class Sabre_DAVServerTest extends PHPUnit_Framework_TestCase {
 
     }
 
-    function request(Sabre_HTTP_Request $r) {
-
-        $this->server->httpRequest = $r;
-        $this->server->httpResponse = new Sabre_HTTP_ResponseMock();
-        $this->server->exec();
-
-        return $this->server->httpResponse;
-
-    }
 
     function assertHTTPStatus($expectedStatus, Sabre_HTTP_Request $req) {
 

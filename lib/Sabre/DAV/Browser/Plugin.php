74944a21c0f60dfbfea4d533f7104a2969867842
Jürgen Henge-Ernst <juergen@henge-ernst.de>
2012-03-01 01:02:15 +0100

    use calculated $mime instead of string 'image/vnd.microsoft.icon'

--- lib/Sabre/DAV/Browser/Plugin.php
+++ lib/Sabre/DAV/Browser/Plugin.php
@@ -473,7 +473,7 @@ class Sabre_DAV_Browser_Plugin extends Sabre_DAV_ServerPlugin {
 
         }
 
-        $this->server->httpResponse->setHeader('Content-Type','image/vnd.microsoft.icon');
+        $this->server->httpResponse->setHeader('Content-Type', $mime);
         $this->server->httpResponse->setHeader('Content-Length', filesize($assetPath));
         $this->server->httpResponse->setHeader('Cache-Control', 'public, max-age=1209600');
         $this->server->httpResponse->sendStatus(200);

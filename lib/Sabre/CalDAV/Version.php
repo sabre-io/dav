5626ee7bde23b644459ed4d398a4b6813d85a1e7
Evert Pot <evert@rooftopsolutions.nl>
2012-03-02 18:03:46 +0100

    Marked CalDAV package as 1.7.0-alpha

--- lib/Sabre/CalDAV/Version.php
+++ lib/Sabre/CalDAV/Version.php
@@ -14,11 +14,11 @@ class Sabre_CalDAV_Version {
     /**
      * Full version number
      */
-    const VERSION = '1.6.1';
+    const VERSION = '1.7.0';
 
     /**
      * Stability : alpha, beta, stable
      */
-    const STABILITY = 'stable';
+    const STABILITY = 'alpha';
 
 }

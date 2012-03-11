aef9c2b46c293fdb0f6d207cc69d02ce325bae8c
Evert Pot <evert@rooftopsolutions.nl>
2012-03-11 19:58:21 +0100

    Bumped versions

--- lib/Sabre/DAV/Version.php
+++ lib/Sabre/DAV/Version.php
@@ -14,7 +14,7 @@ class Sabre_DAV_Version {
     /**
      * Full version number
      */
-    const VERSION = '1.6.1';
+    const VERSION = '1.6.2';
 
     /**
      * Stability : alpha, beta, stable

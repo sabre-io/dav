8e00da3669241956cad21c49fdffdc0298d65992
Evert Pot <evert@rooftopsolutions.nl>
2012-10-12 17:11:29 +0200

    Added Sabre_DAVACL_IPrincipalCollection

--- lib/Sabre/DAVACL/Plugin.php
+++ lib/Sabre/DAVACL/Plugin.php
@@ -598,7 +598,7 @@ class Sabre_DAVACL_Plugin extends Sabre_DAV_ServerPlugin {
         foreach($uris as $uri) {
 
             $principalCollection = $this->server->tree->getNodeForPath($uri);
-            if (!$principalCollection instanceof Sabre_DAVACL_AbstractPrincipalCollection) {
+            if (!$principalCollection instanceof Sabre_DAVACL_IPrincipalCollection) {
                 // Not a principal collection, we're simply going to ignore
                 // this.
                 continue;

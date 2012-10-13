c085725939f5d42b274647f6979c79930cc42b5c
Evert Pot <evert@rooftopsolutions.nl>
2012-10-13 16:43:49 +0200

    Added missing break

--- bin/sabredav.php
+++ bin/sabredav.php
@@ -28,13 +28,14 @@ if (php_sapi_name()!=='cli-server') {
 
 
 $paths = array(
-    __DIR__ . '/../../../autoload.php',
     __DIR__ . '/../vendor/autoload.php',
+    __DIR__ . '/../../../autoload.php',
 );
 
 foreach($paths as $path) {
     if (file_exists($path)) {
         include $path;
+        break;
     }
 }
 

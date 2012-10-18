9bb70aaaf4b9b541fcc37196f71d3ced5a0e095c
Evert Pot <evert@rooftopsolutions.nl>
2012-10-18 14:23:35 +0200

    Bumped version

--- lib/Sabre/CalDAV/Version.php
+++ lib/Sabre/CalDAV/Version.php
@@ -14,7 +14,7 @@ class Sabre_CalDAV_Version {
     /**
      * Full version number
      */
-    const VERSION = '1.7.2';
+    const VERSION = '1.8.0';
 
     /**
      * Stability : alpha, beta, stable

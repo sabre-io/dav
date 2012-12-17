095679f482d7e791aa53e25e469d837199551706
Evert Pot <evert@rooftopsolutions.nl>
2012-12-17 20:35:21 +0100

    Mac Office 2011 does not like the extra namespace declarations.
    Issue #303.

--- lib/Sabre/DAV/Property/SupportedLock.php
+++ lib/Sabre/DAV/Property/SupportedLock.php
@@ -45,27 +45,27 @@ class Sabre_DAV_Property_SupportedLock extends Sabre_DAV_Property {
 
         if (!$this->supportsLocks) return null;
 
-        $lockEntry1 = $doc->createElementNS('DAV:','d:lockentry');
-        $lockEntry2 = $doc->createElementNS('DAV:','d:lockentry');
+        $lockEntry1 = $doc->createElement('d:lockentry');
+        $lockEntry2 = $doc->createElement('d:lockentry');
 
         $prop->appendChild($lockEntry1);
         $prop->appendChild($lockEntry2);
 
-        $lockScope1 = $doc->createElementNS('DAV:','d:lockscope');
-        $lockScope2 = $doc->createElementNS('DAV:','d:lockscope');
-        $lockType1 = $doc->createElementNS('DAV:','d:locktype');
-        $lockType2 = $doc->createElementNS('DAV:','d:locktype');
+        $lockScope1 = $doc->createElement('d:lockscope');
+        $lockScope2 = $doc->createElement('d:lockscope');
+        $lockType1 = $doc->createElement('d:locktype');
+        $lockType2 = $doc->createElement('d:locktype');
 
         $lockEntry1->appendChild($lockScope1);
         $lockEntry1->appendChild($lockType1);
         $lockEntry2->appendChild($lockScope2);
         $lockEntry2->appendChild($lockType2);
 
-        $lockScope1->appendChild($doc->createElementNS('DAV:','d:exclusive'));
-        $lockScope2->appendChild($doc->createElementNS('DAV:','d:shared'));
+        $lockScope1->appendChild($doc->createElement('d:exclusive'));
+        $lockScope2->appendChild($doc->createElement('d:shared'));
 
-        $lockType1->appendChild($doc->createElementNS('DAV:','d:write'));
-        $lockType2->appendChild($doc->createElementNS('DAV:','d:write'));
+        $lockType1->appendChild($doc->createElement('d:write'));
+        $lockType2->appendChild($doc->createElement('d:write'));
 
         //$frag->appendXML('<d:lockentry><d:lockscope><d:exclusive /></d:lockscope><d:locktype><d:write /></d:locktype></d:lockentry>');
         //$frag->appendXML('<d:lockentry><d:lockscope><d:shared /></d:lockscope><d:locktype><d:write /></d:locktype></d:lockentry>');

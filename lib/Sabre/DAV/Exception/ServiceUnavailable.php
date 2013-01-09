4fbb12491e219e9652c90a25d89d1bf4aab81e8f
Thomas Mueller <thomas.mueller@tmit.eu>
2013-01-09 13:34:02 +0100

    fixing copyright use correct exception base class

--- lib/Sabre/DAV/Exception/ServiceUnavailable.php
+++ lib/Sabre/DAV/Exception/ServiceUnavailable.php
@@ -10,12 +10,12 @@ use Sabre\DAV;
  * This exception is thrown in case the service
  * is currently not available (e.g. down for maintenance).
  *
- * @author Thomas Müller
- * @copyright 2013 Thomas Müller <thomas.mueller@tmit.eu>
+ * @author Thomas Müller <thomas.mueller@tmit.eu>
+ * @copyright Copyright (C) 2007-2013 Rooftop Solutions. All rights reserved.
  * @license http://code.google.com/p/sabredav/wiki/License Modified BSD License
  */
 
-class ServiceUnavailable extends Sabre_DAV_Exception {
+class ServiceUnavailable extends DAV\Exception {
 
 	/**
 	 * Returns the HTTP statuscode for this exception

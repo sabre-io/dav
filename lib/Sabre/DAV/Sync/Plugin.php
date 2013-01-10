29c6548833ffdd6ae7f5a5ad9d5be77817644c22
Evert Pot <evert@rooftopsolutions.nl>
2013-01-11 00:24:57 +0100

    Bumped copyright year

--- lib/Sabre/DAV/Sync/Plugin.php
+++ lib/Sabre/DAV/Sync/Plugin.php
@@ -10,7 +10,7 @@ use Sabre\DAV;
  * The sync capabilities only work with collections that implement
  * Sabreu\DAV\Sync\ISyncCollection.
  *
- * @copyright Copyright (C) 2007-2012 Rooftop Solutions. All rights reserved.
+ * @copyright Copyright (C) 2007-2013 Rooftop Solutions. All rights reserved.
  * @author Evert Pot (http://www.rooftopsolutions.nl/)
  * @license http://code.google.com/p/sabredav/wiki/License Modified BSD License
  */

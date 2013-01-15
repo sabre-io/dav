ec1b109482cd7468978cf51fb44d10fe3c7c9020
Evert Pot <evert@rooftopsolutions.nl>
2013-01-15 03:39:44 +0100

    Small steps towards sync support in caldav backend

--- lib/Sabre/CalDAV/Backend/PDO.php
+++ lib/Sabre/CalDAV/Backend/PDO.php
@@ -16,7 +16,7 @@ use Sabre\DAV;
  * @author Evert Pot (http://www.rooftopsolutions.nl/)
  * @license http://code.google.com/p/sabredav/wiki/License Modified BSD License
  */
-class PDO extends AbstractBackend {
+class PDO extends AbstractBackend implements SyncSupport {
 
     /**
      * We need to specify a max date, because we need to stop *somewhere*

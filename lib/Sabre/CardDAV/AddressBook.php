f27a7fa769a827eab386735a6c22bbce7182c631
Evert Pot <evert@rooftopsolutions.nl>
2013-03-06 13:57:36 +0100

    Using getOwner to determine the privileges.

--- lib/Sabre/CardDAV/AddressBook.php
+++ lib/Sabre/CardDAV/AddressBook.php
@@ -268,12 +268,12 @@ class AddressBook extends DAV\Collection implements IAddressBook, DAV\IPropertie
         return [
             [
                 'privilege' => '{DAV:}read',
-                'principal' => $this->addressBookInfo['principaluri'],
+                'principal' => $this->getOwner(),
                 'protected' => true,
             ],
             [
                 'privilege' => '{DAV:}write',
-                'principal' => $this->addressBookInfo['principaluri'],
+                'principal' => $this->getOwner(),
                 'protected' => true,
             ],
 

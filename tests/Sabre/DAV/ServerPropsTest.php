4474bcb9ef87b8681f6d03e8b1f66fd3316e8cf9
Evert Pot <evert@rooftopsolutions.nl>
2013-06-06 12:26:05 +0100

    PHP 5.3 compat.

--- tests/Sabre/DAV/ServerPropsTest.php
+++ tests/Sabre/DAV/ServerPropsTest.php
@@ -54,9 +54,9 @@ class Sabre_DAV_ServerPropsTest extends Sabre_DAV_AbstractServer {
         $this->server->subscribeEvent('beforeGetPropertiesForPath', function($path, $properties, $depth) use ($self, &$hasFired) {
 
             $hasFired = true;
-            $this->assertEquals('', $path);
-            $this->assertEquals(array(), $properties);
-            $this->assertEquals(0, $depth);
+            $self->assertEquals('', $path);
+            $self->assertEquals(array(), $properties);
+            $self->assertEquals(0, $depth);
 
         });
 

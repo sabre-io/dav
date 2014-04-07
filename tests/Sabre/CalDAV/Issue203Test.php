0ed82799c39753516773ec4eaef8c667141207fd
Evert Pot <evert@rooftopsolutions.nl>
2014-04-07 01:06:59 -0400

    More updated license urls.

--- tests/Sabre/CalDAV/Issue203Test.php
+++ tests/Sabre/CalDAV/Issue203Test.php
@@ -10,7 +10,7 @@ use Sabre\VObject;
  *
  * @copyright Copyright (C) 2007-2014 Rooftop Solutions. All rights reserved.
  * @author Evert Pot (http://evertpot.com/)
- * @license http://code.google.com/p/sabredav/wiki/License Modified BSD License
+ * @license http://sabre.io/license/ Modified BSD License
  */
 class Issue203Test extends \Sabre\DAVServerTest {
 
--- tests/Sabre/DAVServerTest.php
+++ tests/Sabre/DAVServerTest.php
@@ -22,7 +22,7 @@ require_once 'Sabre/CardDAV/Backend/Mock.php';
  *
  * @copyright Copyright (C) 2007-2014 fruux GmbH (https://fruux.com/).
  * @author Evert Pot (http://evertpot.com/)
- * @license http://code.google.com/p/sabredav/wiki/License Modified BSD License
+ * @license http://sabre.io/license/ Modified BSD License
  */
 abstract class DAVServerTest extends \PHPUnit_Framework_TestCase {
 

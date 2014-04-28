b73b22515b9cd736f91f9accdebd5290fa2f0f01
jknockaert <jasper@knockaert.nl>
2014-04-28 21:40:51 +0200

    Update CorePlugin.php
    formatting

--- lib/Sabre/DAV/CorePlugin.php
+++ lib/Sabre/DAV/CorePlugin.php
@@ -168,16 +168,16 @@ class CorePlugin extends ServerPlugin {
             // New read/write stream
             $newStream = fopen('php://temp','r+');
 
-			// fseek will return 0 only if $streem is seekable (and -1 otherwise)
+            // fseek will return 0 only if $streem is seekable (and -1 otherwise)
             // for a seekable $body stream we set the pointer write before copying it
             // for a non-seekable $body stream we set the pointer on the copy
-			if ((fseek($body, $start, SEEK_SET)) === 0) {
-				stream_copy_to_stream($body, $newStream, $end-$start+1, $start);
-				rewind($newStream);
-			} else {
-				stream_copy_to_stream($body, $newStream, $end+1);
-				fseek($newStream,$start, SEEK_SET);
-			}
+            if ((fseek($body, $start, SEEK_SET)) === 0) {
+                stream_copy_to_stream($body, $newStream, $end-$start+1, $start);
+                rewind($newStream);
+            } else {
+                stream_copy_to_stream($body, $newStream, $end+1);
+                fseek($newStream,$start, SEEK_SET);
+            }
 
             $response->setHeader('Content-Length', $end-$start+1);
             $response->setHeader('Content-Range','bytes ' . $start . '-' . $end . '/' . $nodeSize);

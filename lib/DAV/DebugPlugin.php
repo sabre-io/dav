3455968540ea5d4e74468a52d2ada56060bf8558
Dominik Tobschall <dominik@fruux.com>
2014-08-05 22:46:22 +0200

    moved to new location

--- lib/DAV/DebugPlugin.php
+++ lib/DAV/DebugPlugin.php
@@ -18,7 +18,7 @@ use
  *
  * @copyright Copyright (C) 2007-2014 fruux GmbH. All rights reserved.
  * @author Evert Pot (http://evertpot.com/)
- * @license http://code.google.com/p/sabredav/wiki/License Modified BSD License
+ * @license http://sabre.io/license/ Modified BSD License
  */
 class DebugPlugin extends ServerPlugin {
 

a02d3ba126459fcd9b2ea55d965be1b663df868f
Armin Hackmann <armin@fruux.com>
2015-01-14 15:19:03 +0100

    add calendars timezone information on expand in ICSExportPlugin

--- tests/Sabre/CalDAV/ExpandEventsFloatingTimeTest.php
+++ tests/Sabre/CalDAV/ExpandEventsFloatingTimeTest.php
@@ -12,6 +12,8 @@ class ExpandEventsFloatingTimeTest extends \Sabre\DAVServerTest {
 
     protected $setupCalDAV = true;
 
+    protected $setupCalDAVICSExport = true;
+
     protected $caldavCalendars = array(
         array(
             'id' => 1,
@@ -138,6 +140,44 @@ END:VCALENDAR
         $response = $this->request($request);
 
         $this->assertEquals(207, $response->getStatus());
+
+        // Everts super awesome xml parser.
+        $body = substr(
+            $response->body,
+            $start = strpos($response->body, 'BEGIN:VCALENDAR'),
+            strpos($response->body, 'END:VCALENDAR') - $start + 13
+        );
+        $body = str_replace('&#13;','',$body);
+
+        $vObject = VObject\Reader::read($body);
+
+        // check if DTSTARTs and DTENDs are correct
+        foreach ($vObject->VEVENT as $vevent) {
+            /** @var $vevent Sabre\VObject\Component\VEvent */
+            foreach ($vevent->children as $child) {
+                /** @var $child Sabre\VObject\Property */
+
+                if ($child->name == 'DTSTART') {
+                    // DTSTART should be the UTC equivalent of given floating time
+                    $this->assertEquals($child->getValue(), '20141108T043000Z');
+                } elseif ($child->name == 'DTEND') {
+                    // DTEND should be the UTC equivalent of given floating time
+                    $this->assertEquals($child->getValue(), '20141108T063000Z');
+                }
+            }
+        }
+    }
+
+    function testExpandExport() {
+
+        $request = new HTTP\Request('GET', '/calendars/user1/calendar1?export&start=1&end=2000000000&expand=1', [
+            'Depth' => 1,
+            'Content-Type' => 'application/xml',
+        ]);
+
+        $response = $this->request($request);
+
+        $this->assertEquals(200, $response->getStatus());
 
         // Everts super awesome xml parser.
         $body = substr(

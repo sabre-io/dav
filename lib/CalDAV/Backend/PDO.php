c5852704d67aa21e47c31baf68b2f8e769b78b97
Thibaud Courtoison <do.not.press.enter@gmail.com>
2015-05-11 11:18:53 +0200

    Fix calendar deletion
    Deleting calendar previously didn't deleted every occurrence of the calendar in the Calendar Changes table, but only the one with the id corresponding to the calendarId (even with no link between them)

--- lib/CalDAV/Backend/PDO.php
+++ lib/CalDAV/Backend/PDO.php
@@ -309,7 +309,7 @@ class PDO extends AbstractBackend implements SyncSupport, SubscriptionSupport, S
         $stmt = $this->pdo->prepare('DELETE FROM '.$this->calendarTableName.' WHERE id = ?');
         $stmt->execute([$calendarId]);
 
-        $stmt = $this->pdo->prepare('DELETE FROM '.$this->calendarChangesTableName.' WHERE id = ?');
+        $stmt = $this->pdo->prepare('DELETE FROM '.$this->calendarChangesTableName.' WHERE calendarid = ?');
         $stmt->execute([$calendarId]);
 
     }

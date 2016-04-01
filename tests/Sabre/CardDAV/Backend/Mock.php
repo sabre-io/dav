5eabd34a7d6003c5fdb1b4ef687cf82b85964e93
Evert Pot <me@evertpot.com>
2016-03-31 21:37:52 -0400

    Strict vCard validation + repair

--- tests/Sabre/CardDAV/Backend/Mock.php
+++ tests/Sabre/CardDAV/Backend/Mock.php
@@ -101,6 +101,25 @@ class Mock extends AbstractBackend {
 
     }
 
+    /**
+     * Returns all cards for a specific addressbook id.
+     *
+     * This method should return the following properties for each card:
+     *   * carddata - raw vcard data
+     *   * uri - Some unique url
+     *   * lastmodified - A unix timestamp
+     *
+     * It's recommended to also return the following properties:
+     *   * etag - A unique etag. This must change every time the card changes.
+     *   * size - The size of the card in bytes.
+     *
+     * If these last two properties are provided, less time will be spent
+     * calculating them. If they are specified, you can also ommit carddata.
+     * This may speed up certain requests, especially with large cards.
+     *
+     * @param mixed $addressbookId
+     * @return array
+     */
     function getCards($addressBookId) {
 
         $cards = [];
@@ -108,34 +127,103 @@ class Mock extends AbstractBackend {
             $cards[] = [
                 'uri'      => $uri,
                 'carddata' => $data,
+                'etag'     => '"' . md5($data) . '"',
+                'size'     => strlen($data)
             ];
         }
         return $cards;
 
     }
 
+    /**
+     * Returns a specfic card.
+     *
+     * The same set of properties must be returned as with getCards. The only
+     * exception is that 'carddata' is absolutely required.
+     *
+     * If the card does not exist, you must return false.
+     *
+     * @param mixed $addressBookId
+     * @param string $cardUri
+     * @return array
+     */
     function getCard($addressBookId, $cardUri) {
 
         if (!isset($this->cards[$addressBookId][$cardUri])) {
             return false;
         }
 
+        $data = $this->cards[$addressBookId][$cardUri];
         return [
             'uri'      => $cardUri,
-            'carddata' => $this->cards[$addressBookId][$cardUri],
+            'carddata' => $data,
+            'etag'     => '"' . md5($data) . '"',
+            'size'     => strlen($data)
         ];
 
     }
 
+    /**
+     * Creates a new card.
+     *
+     * The addressbook id will be passed as the first argument. This is the
+     * same id as it is returned from the getAddressBooksForUser method.
+     *
+     * The cardUri is a base uri, and doesn't include the full path. The
+     * cardData argument is the vcard body, and is passed as a string.
+     *
+     * It is possible to return an ETag from this method. This ETag is for the
+     * newly created resource, and must be enclosed with double quotes (that
+     * is, the string itself must contain the double quotes).
+     *
+     * You should only return the ETag if you store the carddata as-is. If a
+     * subsequent GET request on the same card does not have the same body,
+     * byte-by-byte and you did return an ETag here, clients tend to get
+     * confused.
+     *
+     * If you don't return an ETag, you can just return null.
+     *
+     * @param mixed $addressBookId
+     * @param string $cardUri
+     * @param string $cardData
+     * @return string|null
+     */
     function createCard($addressBookId, $cardUri, $cardData) {
 
         $this->cards[$addressBookId][$cardUri] = $cardData;
+        return '"' . md5($cardData) . '"';
 
     }
 
+    /**
+     * Updates a card.
+     *
+     * The addressbook id will be passed as the first argument. This is the
+     * same id as it is returned from the getAddressBooksForUser method.
+     *
+     * The cardUri is a base uri, and doesn't include the full path. The
+     * cardData argument is the vcard body, and is passed as a string.
+     *
+     * It is possible to return an ETag from this method. This ETag should
+     * match that of the updated resource, and must be enclosed with double
+     * quotes (that is: the string itself must contain the actual quotes).
+     *
+     * You should only return the ETag if you store the carddata as-is. If a
+     * subsequent GET request on the same card does not have the same body,
+     * byte-by-byte and you did return an ETag here, clients tend to get
+     * confused.
+     *
+     * If you don't return an ETag, you can just return null.
+     *
+     * @param mixed $addressBookId
+     * @param string $cardUri
+     * @param string $cardData
+     * @return string|null
+     */
     function updateCard($addressBookId, $cardUri, $cardData) {
 
         $this->cards[$addressBookId][$cardUri] = $cardData;
+        return '"' . md5($cardData) . '"';
 
     }
 

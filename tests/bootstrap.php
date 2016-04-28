6f49d695e19374454eb3a8144f9395bcb3474118
Evert Pot <me@evertpot.com>
2016-04-28 14:56:39 +0700

    Travis postgresql configuration

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -20,7 +20,7 @@ $config = [
     'SABRE_MYSQLDSN'  => 'mysql:host=127.0.0.1;dbname=sabredav_test',
     'SABRE_MYSQLUSER' => 'sabredav',
     'SABRE_MYSQLPASS' => '',
-    'SABRE_PGSQLDSN'  => 'pgsql:host=localhost;dbname=sabredav;user=sabredav;password=sabredav',
+    'SABRE_PGSQLDSN'  => 'pgsql:host=localhost;dbname=sabredav_test;user=sabredav;password=sabredav',
 ];
 
 if (file_exists(__DIR__ . '/config.user.php')) {

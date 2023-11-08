89384d40820a9c7455e46bf367437eb2e4a886a5
Daniel <mail@danielkesselberg.de>
2023-11-08 16:34:40 +0100

    Align createFile signatures with interface (#1422)
    $calendarData => $data
    $vcardData => $data
    $filename => $name
    
    Signed-off-by: Daniel Kesselberg <mail@danielkesselberg.de>

--- lib/CardDAV/AddressBook.php
+++ lib/CardDAV/AddressBook.php
@@ -129,19 +129,19 @@ class AddressBook extends DAV\Collection implements IAddressBook, DAV\IPropertie
      * This method may return an ETag.
      *
      * @param string   $name
-     * @param resource $vcardData
+     * @param resource $data
      *
      * @return string|null
      */
-    public function createFile($name, $vcardData = null)
+    public function createFile($name, $data = null)
     {
-        if (is_resource($vcardData)) {
-            $vcardData = stream_get_contents($vcardData);
+        if (is_resource($data)) {
+            $data = stream_get_contents($data);
         }
         // Converting to UTF-8, if needed
-        $vcardData = DAV\StringUtil::ensureUTF8($vcardData);
+        $data = DAV\StringUtil::ensureUTF8($data);
 
-        return $this->carddavBackend->createCard($this->addressBookInfo['id'], $name, $vcardData);
+        return $this->carddavBackend->createCard($this->addressBookInfo['id'], $name, $data);
     }
 
     /**

4a9d4dc2c0c02e342a9404f438ca413dc203ce36
Thomas Müller <1005065+DeepDiver1975@users.noreply.github.com>
2023-11-09 15:14:12 +0100

    fix: bring code to 2023 :rofl:

--- tests/Sabre/DAV/Locks/MSWord2016Test.php
+++ tests/Sabre/DAV/Locks/MSWord2016Test.php
@@ -1,29 +1,30 @@
-<?php declare (strict_types=1);
+<?php
+
+declare(strict_types=1);
 
 namespace Sabre\DAV\Locks;
 
+use PHPUnit\Framework\TestCase;
 use Sabre\DAV;
 use Sabre\HTTP;
+use Sabre\HTTP\Request;
+use Sabre\TestUtil;
 
-require_once 'Sabre/HTTP/ResponseMock.php';
-require_once 'Sabre/TestUtil.php';
-
-class MSWord2016 extends \PHPUnit_Framework_TestCase {
-
-    function tearDown() {
-
-        \Sabre\TestUtil::clearTempDir();
-
+class MSWord2016Test extends TestCase
+{
+    public function tearDown(): void
+    {
+        TestUtil::clearTempDir();
     }
 
-    function testLockEtc() {
-
-        mkdir(SABRE_TEMPDIR . '/mstest');
-        $tree = new DAV\FS\Directory(SABRE_TEMPDIR . '/mstest');
+    public function testLockEtc(): void
+    {
+        mkdir(SABRE_TEMPDIR.'/mstest');
+        $tree = new DAV\FS\Directory(SABRE_TEMPDIR.'/mstest');
 
         $server = new DAV\Server($tree);
         $server->debugExceptions = true;
-        $locksBackend = new Backend\File(SABRE_TEMPDIR . '/locksdb');
+        $locksBackend = new Backend\File(SABRE_TEMPDIR.'/locksdb');
         $locksPlugin = new Plugin($locksBackend);
         $server->addPlugin($locksPlugin);
 
@@ -32,10 +33,10 @@ class MSWord2016 extends \PHPUnit_Framework_TestCase {
         $server->httpRequest = $this->getLockRequest();
         $server->httpResponse = $response1;
         $server->sapi = new HTTP\SapiMock();
-        $server->exec();
+        $server->start();
 
-        $this->assertEquals(201, $server->httpResponse->getStatus(), 'Full response body:' . $response1->getBodyAsString());
-        $this->assertTrue(!!$server->httpResponse->getHeaders('Lock-Token'));
+        $this->assertEquals(201, $server->httpResponse->getStatus(), 'Full response body:'.$response1->getBodyAsString());
+        $this->assertTrue((bool) $server->httpResponse->getHeader('Lock-Token'));
         $lockToken = $server->httpResponse->getHeader('Lock-Token');
 
         //sleep(10);
@@ -44,29 +45,28 @@ class MSWord2016 extends \PHPUnit_Framework_TestCase {
 
         $server->httpRequest = $this->getLockRequest2();
         $server->httpResponse = $response2;
-        $server->exec();
+        $server->start();
 
         $this->assertEquals(201, $server->httpResponse->status);
-        $this->assertTrue(!!$server->httpResponse->getHeaders('Lock-Token'));
+        $this->assertTrue((bool) $server->httpResponse->getHeader('Lock-Token'));
 
         //sleep(10);
 
         $response3 = new HTTP\ResponseMock();
         $server->httpRequest = $this->getPutRequest($lockToken);
         $server->httpResponse = $response3;
-        $server->exec();
+        $server->start();
 
         $this->assertEquals(204, $server->httpResponse->status);
-
     }
 
-    function getLockRequest() {
-
+    public function getLockRequest(): Request
+    {
         $request = HTTP\Sapi::createFromServerArray([
-           'REQUEST_METHOD'    => 'LOCK',
+           'REQUEST_METHOD' => 'LOCK',
            'HTTP_CONTENT_TYPE' => 'application/xml',
-           'HTTP_TIMEOUT'      => 'Second-3600',
-           'REQUEST_URI'       => '/Nouveau%20Microsoft%20Office%20Excel%20Worksheet.xlsx',
+           'HTTP_TIMEOUT' => 'Second-3600',
+           'REQUEST_URI' => '/Nouveau%20Microsoft%20Office%20Excel%20Worksheet.xlsx',
         ]);
 
         $request->setBody('<D:lockinfo xmlns:D="DAV:">
@@ -82,15 +82,15 @@ class MSWord2016 extends \PHPUnit_Framework_TestCase {
 </D:lockinfo>');
 
         return $request;
-
     }
-    function getLockRequest2() {
 
+    public function getLockRequest2(): Request
+    {
         $request = HTTP\Sapi::createFromServerArray([
-           'REQUEST_METHOD'    => 'LOCK',
+           'REQUEST_METHOD' => 'LOCK',
            'HTTP_CONTENT_TYPE' => 'application/xml',
-           'HTTP_TIMEOUT'      => 'Second-3600',
-           'REQUEST_URI'       => '/~$Nouveau%20Microsoft%20Office%20Excel%20Worksheet.xlsx',
+           'HTTP_TIMEOUT' => 'Second-3600',
+           'REQUEST_URI' => '/~$Nouveau%20Microsoft%20Office%20Excel%20Worksheet.xlsx',
         ]);
 
         $request->setBody('<D:lockinfo xmlns:D="DAV:">
@@ -106,19 +106,17 @@ class MSWord2016 extends \PHPUnit_Framework_TestCase {
 </D:lockinfo>');
 
         return $request;
-
     }
 
-    function getPutRequest($lockToken) {
-
+    public function getPutRequest($lockToken): Request
+    {
         $request = HTTP\Sapi::createFromServerArray([
            'REQUEST_METHOD' => 'PUT',
-           'REQUEST_URI'    => '/Nouveau%20Microsoft%20Office%20Excel%20Worksheet.xlsx',
-           'HTTP_LOCK_TOKEN'        => $lockToken,
+           'REQUEST_URI' => '/Nouveau%20Microsoft%20Office%20Excel%20Worksheet.xlsx',
+           'HTTP_LOCK_TOKEN' => $lockToken,
         ]);
         $request->setBody('FAKE BODY');
+
         return $request;
-
     }
-
 }

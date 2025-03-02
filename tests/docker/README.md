7ab12d429732920282b9006fe2429a68f0437317
Jonathan Boivin <djon2003@hotmail.com>
2025-03-01 21:22:08 -0500

    Add running local repository option
    Signed-off-by: Jonathan Boivin <djon2003@hotmail.com>

--- tests/docker/README.md
+++ tests/docker/README.md
@@ -2,8 +2,24 @@ USAGE
 =====
 
 - Install Docker
+
+REPO
+----
+
 - Download the folder **/tests/docker** from the repository
 - Goto that folder using a terminal
 - Run `docker compose up`
-- Run `docker run -ti sabre-dav-unit-tests`
+- Run `docker run --rm -ti sabre-dav-unit-tests`
 
+LOCAL
+-----
+
+- Goto **$local_repo_path/tests/docker** using a terminal
+- Run `docker compose up`
+- Run `docker run --rm -ti -v $local_repo_path:/test-dir/ sabre-dav-unit-tests`
+
+DEV
+---
+
+- Apply changes to Dockerfile
+- Run `docker compose up --build`
--- tests/docker/run-tests.sh
+++ tests/docker/run-tests.sh
@@ -1,36 +1,56 @@
 #!/bin/bash
 
-repoUrl=
-while [ -z "$repoUrl" ]
-do
-  read -p "Please enter public repository URL:" repoUrl
+function executeTests()
+# $1 = Source directory
+{
+  src="$1"
+  (
+    cd "$src"
+    composer install
+    
+    mkdir -p vendor/sabre/http/tests/www
+    php -S localhost:8000 -t vendor/sabre/http/tests/www &
+    
+    composer phpunit
+  )
+}
+
+function downloadRepo()
+{
+  local repoUrl=
+  while [ -z "$repoUrl" ]
+  do
+    read -p "Please enter public repository URL:" repoUrl
+    
+    if [ -z "$repoUrl" ]; then echo "Repository URL is mandatory"; fi
+  done
   
-  if [ -z "$repoUrl" ]; then echo "Repository URL is mandatory"; fi
-done
+  local branchName=
+  read -p "Enter the branch name [default: master]" branchName
+  branchName=${branchName:-master}
+  
+  echo "Are you sure you want to test with?"
+  echo "URL: $repoUrl"
+  echo "Branch: $branchName"
+  read -p "[y/N]" confirm
+  
+  if [ "$confirm" != "Y" ] && [ "$confirm" != "y" ]; then
+    echo "Cancelling test run"
+    exit
+  fi
+  
+  git clone -b "$branchName" --single-branch --depth 1 "$repoUrl" /src/
+}
 
-read -p "Enter the branch name [default: master]" branchName
-branchName=${branchName:-master}
+echo "Starting test container"
 
-echo "Are you sure you want to test with?"
-echo "URL: $repoUrl"
-echo "Branch: $branchName"
-read -p "[y/N]" confirm
-
-if [ "$confirm" != "Y" ] && [ "$confirm" != "y" ]; then
-  echo "Cancelling test run"
-  exit
+src=/test-dir/
+if [ -d $src ]; then
+  echo "Using local volume" 
+else
+  src=/src/
+  echo "Using repository"
+  downloadRepo
 fi
 
-echo "Starting setup"
-
-git clone -b "$branchName" --single-branch --depth 1 "$repoUrl" /src/
-
-(
-  cd /src/
-  composer install
-  
-  mkdir -p vendor/sabre/http/tests/www
-  php -S localhost:8000 -t vendor/sabre/http/tests/www &
-  
-  composer phpunit
-)
+executeTests "$src"

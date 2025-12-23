b7fd0e73b053f11202ad8857d990d2dc18691a8b
Aleksander Machniak <machniak@kolabsys.com>
2025-12-23 20:37:11 +0100

    CS fix

--- tests/Sabre/DAV/PropFindQuotaTest.php
+++ tests/Sabre/DAV/PropFindQuotaTest.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 
 namespace Sabre\DAV;
 
-Use Sabre\AbstractDAVServerTestCase;
+use Sabre\AbstractDAVServerTestCase;
 use Sabre\Http;
 
 class PropFindQuotaTest extends AbstractDAVServerTestCase

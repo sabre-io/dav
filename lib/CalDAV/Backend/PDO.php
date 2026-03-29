c90791d8cd7530262980501d7bea2eef12f533ec
Abhishek Pai <contact@abhishekpai.co.uk>
2026-03-29 10:39:53 +0200

    #1588: Allow negative linux epoch

--- lib/CalDAV/Backend/PDO.php
+++ lib/CalDAV/Backend/PDO.php
@@ -674,14 +674,6 @@ SQL
                     $lastOccurence = $end->getTimeStamp();
                 }
             }
-
-            // Ensure Occurence values are positive
-            if ($firstOccurence < 0) {
-                $firstOccurence = 0;
-            }
-            if ($lastOccurence < 0) {
-                $lastOccurence = 0;
-            }
         }
 
         // Destroy circular references to PHP will GC the object.

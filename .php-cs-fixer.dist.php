43b7b0838ca1474a45acaeb92ad601b5464dc0d7
Phillip Davis <phil@jankaritech.com>
2026-07-09 21:48:32 +0930

    ci: adjust php-cs-fixer rules identical to other sabre repos

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -3,10 +3,17 @@
 $config = new PhpCsFixer\Config();
 $config->getFinder()
     ->exclude('vendor')
-    ->in(__DIR__);
+    ->in(__DIR__)
+    ->append([
+        __FILE__,
+    ]);
 $config->setRules([
     '@PSR1' => true,
-    '@Symfony' => true
+    '@Symfony' => true,
+    'nullable_type_declaration' => [
+        'syntax' => 'question_mark',
+    ],
+    'nullable_type_declaration_for_default_null_value' => true,
 ]);
 
 return $config;
